385d22a33e4d5b4b7f1f45396187b7a307dce235
Ravern Koh <ravern.koh.dev@gmail.com>
2018-03-30 20:46:43 +0800

    Added selection and toggle

--- app.js
+++ app.js
@@ -1,3 +1,82 @@
+function Looper() {
+  this.selection = -1;
+  this.tracks = [];
+
+  this.elem = $('main .tracks');
+  for (let i = 0; i < 4; i++) {
+    this.addTrack();
+  }
+}
+
+Looper.prototype.toggle = function() {
+  if (this.selection == -1) return;
+  console.log(this.selection);
+};
+
+Looper.prototype.left = function() {
+  if (this.selection < 1) return;
+  this.selection--;
+  this._reselect();
+};
+
+Looper.prototype.right = function() {
+  if (this.selection >= this.tracks.length - 1) return;
+  this.selection++;
+  this._reselect();
+};
+
+Looper.prototype._reselect = function() {
+  for (let i = 0; i < this.tracks.length; i++) {
+    if (i == this.selection) {
+      this.tracks[i].select();
+    } else {
+      this.tracks[i].deselect();
+    }
+  }
+}
+
+Looper.prototype.addTrack = function() {
+  const track = new Track();
+
+  this.elem.append(track.elem);
+
+  const looper = this;
+
+  const selection = this.tracks.length;
+  track.elem.mousemove(function() {
+    looper.selection = selection;
+    looper._reselect();
+  });
+  track.elem.mouseleave(function() {
+    looper.selection = -1;
+    looper._reselect();
+  });
+
+  track.elem.click(function() {
+    looper.toggle();
+  });
+
+  this.tracks.push(track);
+}
+
+
+function Track() {
+  this.empty = true;
+
+  this.elem = $('<div></div>')
+    .addClass('track')
+    .append('record');
+}
+
+Track.prototype.select = function() {
+  this.elem.addClass('selected');
+};
+
+Track.prototype.deselect = function() {
+  this.elem.removeClass('selected');
+};
+
+
 const COLORS = [
   '#7b4b94',
   '#ff3366',
@@ -21,18 +100,3 @@ $(window).keypress(function(e) {
     looper.right();
   }
 });
-
-function Looper() {
-}
-
-Looper.prototype.toggle = function() {
-  console.log('toggle');
-};
-
-Looper.prototype.left = function() {
-  console.log('left');
-};
-
-Looper.prototype.right = function() {
-  console.log('right');
-};
